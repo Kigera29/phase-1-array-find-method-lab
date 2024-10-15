1aa1f9d9d01bce0e7868865e82af6513cbfb331d
Kigera Mark <kigeramark@gmail.com>
2024-10-15 12:19:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// index.js
+function superbowlWin(record) {
+    const winRecord = record.find(entry => entry.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  module.exports = { superbowlWin };
+  
